538c150565c67fc5b72e728c0509908a1469f54e
Emmanuel Martin <ema@vif.fr>
2016-02-11 21:16:11 +0100

    t

--- test/src/main/java/test/model/Grid.java
+++ test/src/main/java/test/model/Grid.java
@@ -5,14 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Grid {
-	public static List<Command> commands = new ArrayList<Command>() {
-		{
-			add(Command.fakeCommand());
-		}
-	};
-	public static List<Drone> drone_s = new ArrayList<Drone>() {{
-		add(Drone.fakeDrone());
-	}};
+	public static List<Command> commands = new ArrayList<Command>();
+	public static List<Drone> drone_s = new ArrayList<Drone>();
 	public static List<Warehouse> warehouses = new ArrayList<>();
 	public static List<Item> items = new ArrayList<>();
 	public static int rows;
