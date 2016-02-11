40bd7d22f2ea39a019b9bd75788e20f2b0b28473
Emmanuel Martin <ema@vif.fr>
2016-02-11 19:50:14 +0100

    update main

--- test/src/main/java/test/Main.java
+++ test/src/main/java/test/Main.java
@@ -5,13 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import test.model.Grid;
+import test.output.InstructionWriter;
 
 public class Main {
 	
 	public static void main(String[] tt) {
 		
 		Parser.parse();
+		Algo.algo();
 		
-		
+		// output
+		Grid.drone_s.stream().forEach(drone -> {InstructionWriter.flush(drone.instructions)});
 	}
 }
