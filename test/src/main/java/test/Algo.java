acefd8626a487f09b6cfd3cea5f72df4d0ed525b
Emmanuel Martin <ema@vif.fr>
2016-02-11 19:36:17 +0100

    add algo

--- test/src/main/java/test/Algo.java
+++ test/src/main/java/test/Algo.java
@@ -1,7 +1,12 @@
 package test;
 
+import java.util.Map.Entry;
+
 import test.model.Command;
+import test.model.Drone;
 import test.model.Grid;
+import test.model.Item;
+import test.model.Warehouse;
 
 public class Algo {
 
@@ -10,7 +15,28 @@ public class Algo {
 	}
 	
 	public static void resolveCommand(Command command) {
-		
+		for (Entry<Item, Long> entry : command.itemToQuantity.entrySet()) {
+			Item item = entry.getKey();
+			Long quantity = entry.getValue();
+			long remainsQty = quantity;
+			while (remainsQty > 0) {
+				Pair<Warehouse, Long> warehouseToQty = findWarehouseFor(item, quantity);
+				Drone drone = findAvailableDrone();
+				drone.deliver(warehouseToQty.first, warehouseToQty.second);
+				remainsQty = remainsQty - warehouseToQty.second;
+			}
+		}
 	}
 	
+	public static Drone findAvailableDrone() {
+		todo;
+	}
+	
+	/**
+	 * return Warehouse to quantity
+	 */
+	public static Pair<Warehouse, Long> findWarehouseFor(Item item, Long quantity) {
+		todo;
+	}
+	 
 }
---
+++ test/src/main/java/test/Pair.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Pair<V, T> {
+public V first;
+public T second;
+public Pair(V first, T second) {
+	super();
+	this.first = first;
+	this.second = second;
+}
+
+}
