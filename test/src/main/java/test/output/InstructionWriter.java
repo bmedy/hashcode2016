0b2431a703a6cd8e1df8764d275ba8c634c4f7f8
Dany Lecoq <dany.lecoq@vif.fr>
2016-02-11 19:35:52 +0100

    fuck

---
+++ test/src/main/java/test/output/InstructionWriter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016 by VIF (Vignon Informatique France)
+ * Project : test
+ * File : $RCSfile$
+ * Created on 11 févr. 2016 by danyl
+ */
+package test.output;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InstructionWriter {
+
+    public static void flush(List<Instruction> instructions) throws IOException {
+
+        List<String> output = new ArrayList<>();
+        DeliverInstruction deliver;
+        LoadInstruction load;
+        WaitInstruction wait;
+
+        for (Instruction inst : instructions) {
+
+            if (inst.type.equals(Instruction.DELIVER)) {
+                deliver = (DeliverInstruction) inst;
+                output.add(inst.droneID + "D" + deliver.orderID + deliver.productID + deliver.productCount);
+            } else if (inst.type.equals(Instruction.LOAD)) {
+                load = (LoadInstruction) inst;
+                output.add(inst.droneID + "L" + load.warehouseID + load.productID + load.productCount);
+            } else if (inst.type.equals(Instruction.WAIT)) {
+                wait = (WaitInstruction) inst;
+                output.add(inst.droneID + "W" + wait.turnCount);
+            }
+        }
+
+        String outFilePath = "/tmp/file.out";
+        Files.write(Paths.get(outFilePath), output);
+
+    }
+
+}
