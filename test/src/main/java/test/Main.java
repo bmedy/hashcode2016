4d94f24686733f5895189d1c2a44089fd3932875
Medy BELMOKHTAR <mbel@vif.fr>
2016-02-11 20:20:12 +0100

    a

--- test/src/main/java/test/Main.java
+++ test/src/main/java/test/Main.java
@@ -1,25 +1,27 @@
 package test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import test.model.Grid;
 import test.output.InstructionWriter;
 
 public class Main {
 	
-	public static void runFile(Path path) {
-		Parser.parse(path);
+	public static void runFile(String path) {
+		try {
+			Parser.parse(path);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Algo.algo();
 		// output
 		Grid.drone_s.stream().forEach(drone -> InstructionWriter.flush(drone.instructions));
 	}
 	
 	public static void main(String[] tt) {
-		runFile(Paths.get("busy_day.in"));
+		runFile("busy_day.in");
 		//runFile(Paths.get("mother_of_all_warehouses.in"));
 		//runFile(Paths.get("redundancy.in"));
 	}
--- test/src/main/java/test/Parser.java
+++ test/src/main/java/test/Parser.java
@@ -3,8 +3,11 @@ package test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import test.model.Grid;
 import test.model.Item;
@@ -12,12 +15,13 @@ import test.model.Warehouse;
 
 public class Parser {
 
-	public static void parse(Path path){
+	public static void parse(String path) throws IOException{
 		
-		try (BufferedReader br = new BufferedReader(new FileReader("/tmp/example.in")))
-		{
+		List<String> lines = Files.readAllLines(Paths.get("resources/" + path));
+		Iterator<String> it = lines.stream().iterator();
+		
 			// line 1
-			String[] line = br.readLine().split("\\s+");
+			String[] line = it.next().split("\\s+");
 			Grid.rows = Integer.valueOf(line[0]);
 			Grid.columns = Integer.valueOf(line[1]);
 			Grid.drones = Integer.valueOf(line[2]);
@@ -26,31 +30,26 @@ public class Parser {
 			
 			// line 2
 			Grid.items = new ArrayList<>();
-			for(int i = 0; i < Integer.valueOf(br.readLine()); i++){
+			for(int i = 0; i < Integer.valueOf(it.next()); i++){
 				Grid.items.add(new Item(i));
 			}
 			
 			// line 3
-			line = br.readLine().split("\\s+");
+			line = it.next().split("\\s+");
 			Grid.items.get(0).weigth = Integer.valueOf(line[0]);
 			Grid.items.get(1).weigth = Integer.valueOf(line[1]);
 			Grid.items.get(2).weigth = Integer.valueOf(line[2]);
 			
 			// line 4
 			Grid.warehouses = new ArrayList<>();
-			for(int i = 0; i < Integer.valueOf(br.readLine()); i++){
-				line = br.readLine().split("\\s+");
+			for(int i = 0; i < Integer.valueOf(it.next()); i++){
+				line = it.next().split("\\s+");
 				Warehouse w = new Warehouse(i);
 				w.r = Integer.valueOf(line[0]);
 				w.c = Integer.valueOf(line[1]);
-				Grid.warehouses.add();
+				// TODO quantity
+				Grid.warehouses.add(w);
 			}
 			
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-
 	}
 }
